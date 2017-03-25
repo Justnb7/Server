a8eb79182c60288e11706055884648d2cb60eeb7
Justnb7 <justinb1208@gmail.com>
2017-03-25 01:45:40 +0000

    Changed .bars

--- NEW DEDI RUN.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-title Server [%date%] [%time%]
-"C:\Program Files\Java\jdk1.8.0_66\bin\java.exe" -Xmx8069m  -cp bin;lib/mvgate3.jar;deps/commons-io-2.4.jar;deps/commons-lang3-3.3.2.jar;lib/Motivote-server.jar;lib/guava-17.0.jar;lib/GTLVote.jar;deps/collisionmap.jar;deps/slf4j-nop.jar;deps/slf4j.jar;deps/gson-2.2.4.jar;deps/netty-all-4.0.25.Final.jar;deps/mysql.jar;deps/mysql-connector-java-5.1.6-bin;deps/mvgate2.jar server.Server
-pause
